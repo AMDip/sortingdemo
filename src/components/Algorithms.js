83ecf12b9a3a9074c423a144a03bb05397b708bc
DipAlvaro <alvaro.dip@logis9.com>
2020-07-07 23:25:30 -0300

    Added some styling to the page with css and also added quick sort to algorithms

--- src/components/Algorithms.js
+++ src/components/Algorithms.js
@@ -1,41 +1,63 @@
 export function MergeSortAlgorithm(array){
     const sortedArray = array.slice();
     const tempArray= array.slice();
-    MergeSort(sortedArray, tempArray, 0, sortedArray.length-1);
-    return sortedArray;
+    const animations=[]
+
+    MergeSort(sortedArray, tempArray, 0, sortedArray.length-1, animations);
+    let sortedAnimation={
+        sortedArray: sortedArray,
+        animations: animations
+    }
+    return sortedAnimation;
 }
 
-function MergeSort(mainArray, tempArray, leftStart, rightEnd){
+function MergeSort(mainArray, tempArray, leftStart, rightEnd, animations){
     if(leftStart === rightEnd) return;
     const middleIndx = Math.floor((leftStart + rightEnd) / 2);
 
-    MergeSort(mainArray, tempArray, leftStart, middleIndx);
-    MergeSort(mainArray, tempArray,  middleIndx + 1, rightEnd);
-    DoMerge(mainArray, tempArray, leftStart, rightEnd);
+    MergeSort(mainArray, tempArray, leftStart, middleIndx, animations);
+    MergeSort(mainArray, tempArray,  middleIndx + 1, rightEnd, animations);
+    DoMerge(mainArray, tempArray, leftStart, rightEnd, animations);
 }
 
-function DoMerge(mainArray, tempArray, leftStart, rightEnd){
+function DoMerge(mainArray, tempArray, leftStart, rightEnd, animations){
     let leftEnd= Math.floor( (leftStart + rightEnd)/ 2 );
     let rightStart= leftEnd + 1;
     let indx= leftStart;
 
     while(leftStart <= leftEnd && rightStart <= rightEnd){
+        // pushing animation indices twice, one to change the color of the indx
+        // that are being compared and one for changing the color back
+        animations.push([leftStart, rightStart]);
+        animations.push([leftStart, rightStart]);
         if(tempArray[leftStart] <= tempArray[rightStart]){
             mainArray[indx] = tempArray[leftStart];
+            animations.push([indx, mainArray[indx]]);
             leftStart++;
         }else{
             mainArray[indx] = tempArray[rightStart];
+            animations.push([indx, mainArray[indx]]);
             rightStart++;
         }
         indx++;
     }
     while(leftStart <= leftEnd){
+        // pushing animation indices twice, one to change the color of the indx
+        // that are being compared and one for changing the color back
+        animations.push([leftStart, leftEnd]);
+        animations.push([leftStart, leftEnd]);
         mainArray[indx] = tempArray[leftStart];
+        animations.push([indx, mainArray[indx]]);
         leftStart++;
         indx++;
     }
     while(rightStart <= rightEnd){
+        // pushing animation indices twice, one to change the color of the indx
+        // that are being compared and one for changing the color back
+        animations.push([rightStart, rightEnd]);
+        animations.push([rightStart, rightEnd]);
         mainArray[indx] = tempArray[rightStart];
+        animations.push([indx, mainArray[indx]]);
         rightStart++;
         indx++;
     }
@@ -45,3 +67,93 @@ function DoMerge(mainArray, tempArray, leftStart, rightEnd){
 function arrayCopy(src, srcIndex, dest, destIndex, length) {
     dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
   }
+
+export function bubbleSortAlgo(array){
+    let animations=[];
+    let aux=[];
+
+    for (let i=0; i < array.length-1; i++) {
+        for (let j = 0; j < array.length-i-1; j++) {
+          // pushing animation indices twice, one to change the color of the indx
+          // that are being compared and one for changing the color back
+          animations.push([j,j+1])
+          animations.push([j,j+1])
+          if (array[j] > array[j+1]) {
+            aux = array[j];
+            array[j] = array[j+1];
+            array[j+1] = aux;
+          }
+          // every 3rd row, we need to update div's indxs with new height
+          animations.push([j, array[j], j+1 ,array[j+1]]);
+        }
+      }
+
+      let sortedAnimation={
+          sortedArray: array,
+          animations: animations
+      };
+      return sortedAnimation;
+}
+
+export function quickSortAlgo(array){
+    let sortedArray=array.slice();
+    let animations=[];
+    quickSort(array, 0, array.length - 1, animations);
+    let sortedAnimation={
+        sortedArray: sortedArray,
+        animations: animations
+    }
+    return sortedAnimation;
+}
+
+function quickSort(array, startIndx, endIndx, animations){
+    if(startIndx >= endIndx) { 
+        return;
+    }
+    // selecting pivot to be the value from the middle of the array, but 
+    // it could be any other value of the array
+    let pivot = array[Math.floor((startIndx + endIndx) / 2)];
+    let partitionPosition = partition(array, startIndx, endIndx, pivot, animations);
+    
+    quickSort(array, startIndx, partitionPosition, animations);
+    quickSort(array, partitionPosition+1, endIndx, animations);
+}
+
+function partition(array, startIndx, endIndx, pivot, animations){
+    let i=startIndx;
+    let j=endIndx; 
+    let pivotindx= Math.floor((startIndx + endIndx) / 2);
+
+    // iterate as long as start position is not less than end position
+    while (i <= j){
+        // checks if the values on the left of the pivot are 
+        // less than the pivot's value
+        while(array[i] < pivot){
+            animations.push([i, pivotindx]);
+            animations.push([i, pivotindx]);
+            animations.push([i, array[i], j, array[j]]);
+            i++; 
+        }
+        // checks if the values on the right of the pivot are 
+        // less than the pivot's value
+        while(array[j] > pivot){
+            animations.push([j, pivotindx]);
+            animations.push([j, pivotindx]);
+            animations.push([i, array[i], j, array[j]]);
+            j--; 
+        }
+        if(i <= j){ 
+            animations.push([i, j]);
+            animations.push([i, j]);
+            // with this swap we move values to the correct side of the pivot
+            [array[i], array[j]] = [array[j], array[i]];
+            animations.push([i, array[i], j, array[j]]);
+            //after swapping, we need to continue with the next left and right position
+            i++;
+            j--;
+        }
+    }
+    // returning the index of the sorted pivot position of this partition,
+    // indx i would be 1 position ahead after the last swap
+    return --i;
+}
--- src/components/VisualSorts.jsx
+++ src/components/VisualSorts.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./VisualSorts.css";
-import { MergeSortAlgorithm } from "./Algorithms";
+import { MergeSortAlgorithm,
+         quickSortAlgo,
+         bubbleSortAlgo } from "./Algorithms";
 
 const MAX_BAR_SIZE = 500;
-const NUMBER_OF_BARS = 50;
+const NUMBER_OF_BARS = 80;
 
 // Speed of the animations.
-const ANIMATION_SPEED_MS = 100;
+const ANIMATION_SPEED_MS = 50;
 
 // Main color of the array bars.
 const PRIMARY_COLOR = "blueviolet";
@@ -24,10 +26,8 @@ export default class VisualSorts extends React.Component {
 
   componentDidMount() {
     this.createBars();
-    testSorting();
-    //testing merge sort algorithm
-    // let test = [4, 6, 2, 3, 9, 5, 7, 8, 0, 1]
-    // MergeSortAlgorithm(test);
+    //this method tests the sortings algorithm against large unsorted arrays
+    //testSorting();
   }
 
   createBars() {
@@ -38,28 +38,96 @@ export default class VisualSorts extends React.Component {
     this.setState({ bars: bars });
   }
 
+  refreshBars(){
+    window.location.reload(false);
+  }
+
+  mergeSort(){
+    const array = this.state.bars.slice();
+    const bar = document.getElementsByClassName("bar-1");
+    const {animations} = MergeSortAlgorithm(array);
+    console.log(animations);
+    //this.setState({bars: sortedArray});
+    for(let i=0; i < animations.length; i++){
+      //If the modulus of 3 is equal to 2, this means that its the 3dr iteration
+      //and we need to change the size of the bars if they are different
+      if(i%3 === 2){
+        const [index1, height1] = animations[i];
+        const barStyle1 = bar[index1].style;
+        setTimeout( () => {
+        barStyle1.height = `${height1}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }else{
+        //if modulus of 3 equal 0, this is the 1st iteration, so we change color for comparison
+        if(i%3 === 0){
+          const [index1, index2] = animations[i];
+          const barStyle1 = bar[index1].style;
+          const barStyle2 = bar[index2].style;
+          setTimeout( () => {
+          barStyle1.backgroundColor = SECONDARY_COLOR;
+          barStyle2.backgroundColor = SECONDARY_COLOR;
+          }, i * ANIMATION_SPEED_MS);
+        }else{
+          //if modulus of 3 equal 1, this is the 2st iteration, so we change color back
+          const [index1, index2] = animations[i];
+          const barStyle1 = bar[index1].style;
+          const barStyle2 = bar[index2].style;  
+          setTimeout( () => {
+          barStyle1.backgroundColor = PRIMARY_COLOR;
+          barStyle2.backgroundColor = PRIMARY_COLOR;
+          }, i * ANIMATION_SPEED_MS);
+        }
+     }
+    }
+  }
+
+  quickSort(){
+    //let array=[3,5,8,9,3,6,15,12,16];
+    const array = this.state.bars.slice();
+    //this.setState({bars: array});
+    const bar = document.getElementsByClassName("bar-1");
+    const {animations} = quickSortAlgo(array);
+    for(let i=0; i < animations.length; i++){
+      //If the modulus of 3 is equal to 2, this means that its the 3dr iteration
+      //and we need to change the size of the bars if they are different
+      if(i%3 === 2){
+        const [index1, height1, index2, height2] = animations[i];
+        const barStyle1 = bar[index1].style;
+        const barStyle2 = bar[index2].style;
+        setTimeout( () => {
+        barStyle1.height = `${height1}px`;
+        barStyle2.height = `${height2}px`;;
+        }, i * ANIMATION_SPEED_MS);
+      }else{
+        //if modulus of 3 equal 0, this is the 1st iteration, so we change color for comparison
+        if(i%3 === 0){
+          const [index1, index2] = animations[i];
+          const barStyle1 = bar[index1].style;
+          const barStyle2 = bar[index2].style;
+          setTimeout( () => {
+          barStyle1.backgroundColor = SECONDARY_COLOR;
+          barStyle2.backgroundColor = SECONDARY_COLOR;
+          }, i * ANIMATION_SPEED_MS);
+        }else{
+          //if modulus of 3 equal 1, this is the 2st iteration, so we change color back
+          const [index1, index2] = animations[i];
+          const barStyle1 = bar[index1].style;
+          const barStyle2 = bar[index2].style;  
+          setTimeout( () => {
+          barStyle1.backgroundColor = PRIMARY_COLOR;
+          barStyle2.backgroundColor = PRIMARY_COLOR;
+          }, i * ANIMATION_SPEED_MS);
+        }
+     }
+    }
+  }
+
   bubbleSort() {
     const array = this.state.bars.slice();
-    let i=0;
-    let j=0;
-    let aux = [];
-    let animations=[];
     const bar = document.getElementsByClassName("bar-1");
+    const {animations} = bubbleSortAlgo(array);
 
-    for (i=0; i < array.length-1; i++) {
-      for (j = 0; j < array.length-i-1; j++) {
-        animations.push([j,j+1])
-        animations.push([j,j+1])
-        if (array[j] > array[j+1]) {
-          aux = array[j];
-          array[j] = array[j+1];
-          array[j+1] = aux;
-        }
-        animations.push([j, array[j], j+1 ,array[j+1]]);
-      }
-    }
-
-    for(i=0; i < animations.length; i++){
+    for(let i=0; i < animations.length; i++){
       //If the modulus of 3 is equal to 2, this means that its the 3dr iteration
       //and we need to change the size of the bars if they are different
       if(i%3 === 2){
@@ -97,6 +165,17 @@ export default class VisualSorts extends React.Component {
   render() {
     return (
       <div>
+        <header>
+          <img className="sort-logo" src={require("../ordenar-2.svg")} alt="logo"></img>
+          <nav className="navbar_sorts">
+            <ul >
+              <li><a href="#" onClick={() => this.bubbleSort()}>Bubble Sort</a></li>
+              <li><a href="#" onClick={() => this.mergeSort()}>Merge Sort</a></li>
+              <li><a href="#" onClick={() => this.quickSort()}>Quick Sort</a></li>
+              <li><a className="refresh" href="#" onClick={()=>this.refreshBars()}>Refresh Bars</a></li>
+            </ul>
+          </nav>
+        </header>
         <div className="bar-container-div">
           {this.state.bars.map((item, index) => (
             <div
@@ -108,11 +187,6 @@ export default class VisualSorts extends React.Component {
             }}></div>
           ))}
         </div>
-          <div>
-            <button onClick={() => this.createBars()}>reset bars</button>
-            <button onClick={() => this.bubbleSort()}>sort bars</button>
-            <button onClick={() => this.mergeSort()}>sort bars</button>
-          </div>
       </div>
     );
   }
@@ -122,7 +196,6 @@ function testSorting(){
   //creating 100 comparisons to test the sort
   for(let i=0; i < 100; i++){
     let array=[]
-    let aux=[];
     let length= createRandomFromInterval(500,1000)
 
     //creates a new random array
@@ -130,22 +203,17 @@ function testSorting(){
       array.push(createRandomFromInterval(-1000,1000));
     }
     const sortedArrayWithMethod = array.slice();
-
-    //bubble sorting algorithm
-    // for (let i=0; i < array.length-1; i++) {
-    //   for (let j = 0; j < array.length-1; j++) {
-    //     if (array[j] > array[j+1]) {
-    //       aux = array[j];
-    //       array[j] = array[j+1];
-    //       array[j+1] = aux;
-    //     }
-    //   }
-    // }
-    let testArray = MergeSortAlgorithm(array);
-    //sorts the array using js sorting method
     sortedArrayWithMethod.sort( (a,b)=> a-b )
+
+    let { sortedArray: mergeSortArray} = MergeSortAlgorithm(array);
+    let { sortedArray: bubbleSortArray} = bubbleSortAlgo(array);
+    let { sortedArray: quickSortArray} = quickSortAlgo(array);
+
+    //sorts the array using js sorting method
     //comparing the two arrays to verify the the created array is sorted correctly
-    console.log(arraysAreEqual(sortedArrayWithMethod, testArray));
+    console.log(arraysAreEqual(sortedArrayWithMethod, mergeSortArray));
+    console.log(arraysAreEqual(sortedArrayWithMethod, bubbleSortArray));
+    console.log(arraysAreEqual(sortedArrayWithMethod, quickSortArray));
   }
 }
 
